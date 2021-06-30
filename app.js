75a209ce668943897c3b4da683f3661b6153ac37
MarahQ <>
2021-06-30 20:18:49 +0000

    please upload

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 var username = prompt("What is your name?");
+var username=
 alert('welcome to the best place for your health')
+var weight = prompt("enter weight in Kg")
+if('60'){
+        alert('you need to gain weight')
+    }
+    else{('your healthy')}
+     
